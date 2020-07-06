fb660cb458abdce85869c4a5f8cc908e50573270
Ruben Cardenas <rcardenas@tacitknowledge.com>
2020-07-06 11:48:43 -0500

    added list of owners. closes #13

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/vets")
-public class VetController {
+@RequestMapping("/owners")
+public class OwnerController {
 
     @RequestMapping({"", "/index", "/index.html"})
     public String Index(){
-        return "vets/index";
+        return "owners/index";
     }
 }
