eb684e79b1bcad73670878bba48ed31db08228fa
Ruben Cardenas <rcardenas@tacitknowledge.com>
2020-07-17 11:06:53 -0500

    added pettypes to bootstrap. Closes #32

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -6,6 +6,8 @@ import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -18,18 +20,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
+    private final PetService petService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, PetService petService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
+        this.petService = petService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("Perro");
+        PetType savedDog = petTypeService.save(dog);
+
         Pet pet = new Pet();
-        PetType petType = new PetType();
-        petType.setName("Perro");
-        pet.setPetType(petType);
+        pet.setPetType(savedDog);
+        Pet savedPet = petService.save(pet);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Ruben");
@@ -37,7 +46,7 @@ public class DataLoader implements CommandLineRunner {
         owner1.setAddress("Dalton 103");
         owner1.setCity("Apodaca");
         owner1.setTelephone("811423490");
-        owner1.setPets(new HashSet<Pet>(Arrays.asList(pet)));
+        owner1.setPets(new HashSet<>(Arrays.asList(savedPet)));
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
@@ -46,7 +55,7 @@ public class DataLoader implements CommandLineRunner {
         owner2.setAddress("Saucito 567");
         owner2.setCity("Chihuahua");
         owner2.setTelephone("614978465");
-        owner2.setPets(new HashSet<Pet>(Arrays.asList(pet)));
+        owner2.setPets(new HashSet<>(Arrays.asList(savedPet)));
         ownerService.save(owner2);
 
         System.out.println("Loaded Owners...");
