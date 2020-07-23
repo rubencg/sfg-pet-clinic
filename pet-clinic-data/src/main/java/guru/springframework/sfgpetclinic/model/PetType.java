05dbcd67e0f024e03b4c99bfed717e7ea31f8637
Ruben Cardenas <rcardenas@macbook-pro.lan>
2020-07-22 19:37:54 -0500

    owner to jpa entity. Closes #36

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,6 +1,14 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntitiy {
+
+    @Column(name = "name")
     private String name;
 
     public String getName() {
