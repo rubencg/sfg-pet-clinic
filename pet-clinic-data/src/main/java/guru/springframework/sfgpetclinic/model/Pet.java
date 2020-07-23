8b4f3c4d24068a5ee31cbde52a887266ed473cfa
Ruben Cardenas <rubencg88@gmail.com>
2020-07-22 19:55:58 -0500

    added visit to jpa entities. Closes #38

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -1,7 +1,10 @@
 package guru.springframework.sfgpetclinic.model;
 
 import javax.persistence.*;
+import java.security.AllPermission;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -19,6 +22,9 @@ public class Pet extends BaseEntitiy {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -50,5 +56,13 @@ public class Pet extends BaseEntitiy {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
